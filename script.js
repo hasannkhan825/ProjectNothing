efb3909b732fdc30374a725f98957acc4417528d
Hasan <75249582+hasannkhan825@users.noreply.github.com>
2022-12-26 13:20:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const elementEl = document.getElementById('openerText');
+
+
+function typeWriter(el) {
+  const textArray = el.innerHTML.split('');
+  el.innerHTML = '';
+  textArray.forEach((letter, i) =>
+    setTimeout(() => (el.innerHTML += letter), 95 * i));
+  // setInterval(() => typeWriter(el), 8000);
+}
+
+typeWriter(elementEl);
